32595c46f397475614eb7085b576b228473438b3
Jorge Pavez <jorge.pavez@stationdomain.com>
2022-04-30 10:54:27 -0400

    Se agregaron nuevas activities para el desarrollo del proyecto.

--- app/src/main/java/com/example/proyectoutem0122/MainActivity.java
+++ app/src/main/java/com/example/proyectoutem0122/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
     public void IrSiguiente(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
         startActivity(intent);
